bd02ce194473a7a0f48abfd51bad449b601c1b68
Henry Seigbein <hensco95@gmail.com>
2024-01-25 14:18:05 +0100

    setup: created tailwind file, added basic styles and meta description

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        limeGreen: "hsl(163, 72%, 41%)",
+        brightRed: "hsl(356, 69%, 56%)",
+        facebook: "hsl(208, 92%, 53%)",
+        twitter: "hsl(203, 89%, 53%)",
+        igGradient1: "hsl(37, 97%, 70%)",
+        igGradient2: "hsl(329, 70%, 58%)",
+        youTube: "hsl(348, 97%, 39%)",
+
+        // dark theme
+        vDarkBlueBg: "hsl(230, 17%, 14%)",
+        vDarkBlueTopBgPattern: "hsl(232, 19%, 15%)",
+        darkDesaturatedBlueCardBg: "hsl(228, 28%, 20%)",
+        desaturatedBlueText: "hsl(228, 34%, 66%)",
+        whiteText: "hsl(0, 0%, 100%)",
+
+        // light theme
+        whiteBg: "hsl(0, 0%, 100%)",
+        lvPaleBlueTopBgPattern: "hsl(225, 100%, 98%)",
+        lightGrayishBlue: "hsl(227, 47%, 96%)",
+        lDarkGrayishBlueText: "hsl(228, 12%, 44%)",
+        lVeryDarkBlueText: "hsl(230, 17%, 14%)",
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
